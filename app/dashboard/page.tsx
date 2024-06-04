9e972890acbad0f27eee9413f13e9368332306ab
Abhishek <abhishekbhosale.dev@gmail.com>
2024-06-04 18:51:32 +0530

    feat: display notes on dashboard page
    show users notes on the dashboard page with edit and delete btn

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,105 @@
-const DashboardPage = () => {
-  return <div>welcome to the dashboard</div>;
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import prisma from "@/app/lib/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { Edit, File } from "lucide-react";
+import { Card } from "@/components/ui/card";
+import { TrashDelete } from "@/components/SubmitButton";
+import { revalidatePath } from "next/cache";
+
+const getData = async (userId: string) => {
+  const data = await prisma.note.findMany({
+    where: {
+      userId: userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return data;
+};
+
+const DashboardPage = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  const data = await getData(user?.id as string);
+
+  async function deleteNote(formData: FormData) {
+    "use server";
+
+    const noteId = formData.get("noteId") as string;
+
+    await prisma.note.delete({
+      where: {
+        id: noteId,
+      },
+    });
+
+    revalidatePath("/dasboard");
+  }
+
+  return (
+    <div className="grid items-start gap-8">
+      <div className="flex items-center justify-between px-2">
+        <div className="grid gap-1">
+          <h1 className="text-3xl md:text-4xl font-semibold">Your Notes</h1>
+        </div>
+
+        <Button asChild>
+          <Link href="/dashboard/new">Creata a new Note</Link>
+        </Button>
+      </div>
+
+      {data.length < 1 ? (
+        <div className="flex min-h-[400px] flex-col items-center justify-center rounded-md border border-dashed p-8 text-center animate-in fade-in-50">
+          <div className="flex h-20 w-20 items-center justify-center rounded-full bg-primary/10">
+            <File className="w-10 h-10 text-primary" />
+          </div>
+
+          <h2 className="mt-6 text-xl font-semibold">
+            You dont have any notes created
+          </h2>
+          <p className="mb-8 mt-2 text-center text-sm leading-6 text-muted-foreground max-w-sm mx-auto">
+            You currently dont have any notes. Please create some so that you
+            can see them right here.
+          </p>
+        </div>
+      ) : (
+        <div className="flex flex-col gap-y-4">
+          {data.map((item) => (
+            <Card
+              key={item.id}
+              className="flex items-center justify-between p-4"
+            >
+              <div>
+                <h1 className="font-semibold text-xl text-primary">
+                  {item.title}
+                </h1>
+                <p>
+                  {new Intl.DateTimeFormat("en-US", {
+                    dateStyle: "full",
+                  }).format(new Date(item.createdAt))}
+                </p>
+              </div>
+              <div className="flex gap-x-4">
+                <Link href={`/dashboard/new/${item.id}`}>
+                  <Button variant="outline" size="icon">
+                    <Edit className="w-4 h-4" />
+                  </Button>
+                </Link>
+                <form action={deleteNote}>
+                  <input type="hidden" name="noteId" value={item.id} />
+                  <TrashDelete />
+                </form>
+              </div>
+            </Card>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default DashboardPage;
--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -66,7 +66,7 @@ const page = async () => {
     <div className="grid items-start gap-8">
       <div className="flex items-center justify-between px-2">
         <div className="grid gap-1">
-          <h1 className="text-3xl md:text-4xl">Settings</h1>
+          <h1 className="text-3xl md:text-4xl font-semibold">Settings</h1>
           <p className="text-lg text-muted-foreground">Your Profile settings</p>
         </div>
       </div>
