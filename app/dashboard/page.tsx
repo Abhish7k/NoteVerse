689aeb067833bd5cfca06d0849b349a8bdf13747
Abhishek <abhishekbhosale.dev@gmail.com>
2024-06-05 23:39:27 +0530

    feat: check subscription status to create new note
    added logic to check for subscription status before allowing user to create a new note

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,12 +8,26 @@ import { TrashDelete } from "@/components/SubmitButton";
 import { revalidatePath } from "next/cache";
 
 const getData = async (userId: string) => {
-  const data = await prisma.note.findMany({
+  // const data = await prisma.note.findMany({
+  //   where: {
+  //     userId: userId,
+  //   },
+  //   orderBy: {
+  //     createdAt: "desc",
+  //   },
+  // });
+
+  const data = await prisma.user.findUnique({
     where: {
-      userId: userId,
+      id: userId,
     },
-    orderBy: {
-      createdAt: "desc",
+    select: {
+      Notes: true,
+      Subscription: {
+        select: {
+          status: true,
+        },
+      },
     },
   });
 
@@ -46,29 +60,54 @@ const DashboardPage = async () => {
         <div className="grid gap-1">
           <h1 className="text-3xl md:text-4xl font-semibold">Your Notes</h1>
         </div>
-
-        <Button asChild>
-          <Link href="/dashboard/new">Creata a new Note</Link>
-        </Button>
+        {data?.Subscription?.status === "active" ? (
+          <Button asChild>
+            <Link href="/dashboard/new">Creata a new Note</Link>
+          </Button>
+        ) : (
+          <Button asChild>
+            <Link href="/dashboard/billing">Creata a new Note</Link>
+          </Button>
+        )}
       </div>
 
-      {data.length < 1 ? (
+      {data?.Notes.length == 0 ? (
         <div className="flex min-h-[400px] flex-col items-center justify-center rounded-md border border-dashed p-8 text-center animate-in fade-in-50">
           <div className="flex h-20 w-20 items-center justify-center rounded-full bg-primary/10">
             <File className="w-10 h-10 text-primary" />
           </div>
 
-          <h2 className="mt-6 text-xl font-semibold">
-            You dont have any notes created
-          </h2>
-          <p className="mb-8 mt-2 text-center text-sm leading-6 text-muted-foreground max-w-sm mx-auto">
-            You currently dont have any notes. Please create some so that you
-            can see them right here.
-          </p>
+          {data?.Subscription?.status === "active" ? (
+            <div>
+              <h2 className="mt-6 text-xl font-semibold">
+                You dont have any notes created
+              </h2>
+              <p className="mb-8 mt-2 text-center text-sm leading-6 text-muted-foreground max-w-sm mx-auto">
+                You currently dont have any notes. Please create some so that
+                you can see them right here.
+              </p>
+              <Button asChild>
+                <Link href="/dashboard/new">Create a new Note</Link>
+              </Button>
+            </div>
+          ) : (
+            <div>
+              <h2 className="mt-6 text-xl font-semibold">
+                You dont have any notes created
+              </h2>
+              <p className="mb-8 mt-2 text-center text-sm leading-6 text-muted-foreground max-w-sm mx-auto">
+                You currently dont have any notes. Please subscribe to premium
+                to create new notes.
+              </p>
+              <Button asChild>
+                <Link href="/dashboard/billing">Please Upgrade to Premium</Link>
+              </Button>
+            </div>
+          )}
         </div>
       ) : (
         <div className="flex flex-col gap-y-4">
-          {data.map((item) => (
+          {data?.Notes.map((item) => (
             <Card
               key={item.id}
               className="flex items-center justify-between p-4"
