a53ba3938c304d0b85b6d5166f8ea35469baea20
Abhishek <abhishekbhosale.dev@gmail.com>
2024-06-25 13:09:27 +0530

    feat: toploader for better ux

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Navbar from "@/components/Navbar";
 import prisma from "@/app/lib/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { unstable_noStore as noStore } from "next/cache";
+import NextTopLoader from "nextjs-toploader";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -52,6 +53,7 @@ export default async function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <NextTopLoader />
           <Navbar />
           {children}
         </ThemeProvider>
